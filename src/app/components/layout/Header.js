966c4d49384cb2db43308e66b19c52a033f8b70b
Victor <andre.victorfr60@gmail.com>
2023-12-04 15:38:47 +0100

    Image header

--- src/app/components/layout/Header.js
+++ src/app/components/layout/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between">
       <nav className="flex items-center gap-4 text-gray-500 font-semibold">
-        <Image src="/icon.ico" width={75} height={75} alt="logo seekPlayer"/>
+        <Image src="/images/seek_player.png" width={75} height={75} alt="logo seekPlayer"/>
         <Link className="text-primary font-semibold text-2xl" href={"/"}>
           SeekPlayer
         </Link>
