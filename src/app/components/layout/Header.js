cc44900336eb61e9fbd836a7f787f1476e03031d
Victor <andre.victorfr60@gmail.com>
2023-12-04 15:33:53 +0100

    Add new metadata

--- src/app/components/layout/Header.js
+++ src/app/components/layout/Header.js
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
@@ -6,6 +7,7 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between">
       <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+        <Image src="/icon.ico" width={75} height={75} alt="logo seekPlayer"/>
         <Link className="text-primary font-semibold text-2xl" href={"/"}>
           SeekPlayer
         </Link>
