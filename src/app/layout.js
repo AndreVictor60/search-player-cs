2a8d39407803cf723d9bad67c6d69f963d1e03e6
Victor <andre.victorfr60@gmail.com>
2023-12-06 16:28:54 +0100

    add new player

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,33 +7,39 @@ const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata = {
   title: "SeekPlayer",
-  description: "Web application to search for a professional counter-strike player",
+  description:
+    "Web application to search for a professional counter-strike player",
   icons: {
     icon: [
       {
-        url: '/icon.ico',
-        href: '/icon.ico',
+        url: "/icon.ico",
+        href: "/icon.ico",
       },
-
     ],
-  }
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true} className={roboto.className}>
+      <body suppressHydrationWarning className={roboto.className}>
         <main className="max-w-7xl mx-auto p-4">
           <Provider attribute="class" defaultTheme="system" enableSystem>
-          <Header />
+            <Header />
             {children}
             <footer className="border-t p-8 text-center text-gray-500 dark:text-red-500 mt-16">
-              &copy; 2023  | Made by <a className="underline" target="_blank" href="https://twitter.com/professorbidou">Professor</a>
+              &copy; 2023 | Made by{" "}
+              <a
+                className="underline"
+                target="_blank"
+                href="https://twitter.com/professorbidou"
+              >
+                Professor
+              </a>
             </footer>
           </Provider>
-
         </main>
       </body>
     </html>
-  )
-};
+  );
+}
